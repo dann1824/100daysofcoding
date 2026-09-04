4d7372c30f31eb6c1c74f0a582ce93648e62ef96
fharr <fhardanaldiansyah7@gmail.com>
2026-09-05 01:56:21 +0800

    DayFourCoding

---
+++ DayFourCoding.java
@@ -0,0 +1,37 @@
+public class DayFourCoding {
+
+  public static void main(String[] args) {
+    System.out.println("Jenis \t Tipe \t Data");
+    
+// Tipe data byte untuk itu di gunakan dalam 
+    //data kecil seperti umur atau suhu
+byte suhuBadanKu = 13;
+// Tipe data short untuk data tahun
+    //atau rentang menengah
+short tahunLahirKu = 2008;
+
+// nah untuk jenis tip data int 
+//untuk jumlah umum seperti harga kah atau stok barang bisa jga
+int hargaBarang = 150000;
+
+// Tipe data long itu untuk nilai besar 
+//seperti nomor identifikasi atau populasi atau 
+//mungkin bisa jarak juga
+long jumlahPenduduk = 1998000000L;
+
+    System.out.println("Suhu: " + suhuBadanKu);
+  System.out.println("Tahun: " + tahunLahirKu);
+        System.out.println("Harga: " + hargaBarang);
+      System.out.println("Penduduk: " + jumlahPenduduk);
+
+    /** 
+    *int 
+      *long 
+      *byte   //---adalah tipe data yang harus di ingat--
+      *short 
+      */
+    
+    
+  }
+}
+    
