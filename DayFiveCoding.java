d904b0357ffa200007cb35824bb1f3f48bebf380
fharr <fhardanaldiansyah7@gmail.com>
2026-09-06 20:24:55 +0800

    Day5Coding

---
+++ DayFiveCoding.java
@@ -0,0 +1,20 @@
+
+
+public class DayFiveCoding {
+    public static void main(String[] args) {
+        //Tipe data float dan double
+        /** float itu bersifat tunggal 6-7 desimal 
+         * terus untuk double itu ganda 15-17 desimal
+         * float harus memakai akhiran f
+         * double opsional
+         */
+         
+        //
+        float suhuNegaraSaatIniMencapai  = 26.5f;
+        double kasusSubsidiMinyakSawitPertaminaMencapaiAngkaHingga = 285000_000_000_000d;
+        System.out.println(">>>KONDISI NEGARA INDONESIA<<<");
+        System.out.println("suhu negara saat ini: " + suhuNegaraSaatIniMencapai + "°C" );
+        System.out.println("kasus korupsi terbanyak Rp : " + kasusSubsidiMinyakSawitPertaminaMencapaiAngkaHingga + "trilliun");
+    }
+    
+}
