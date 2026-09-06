8bb5f4d97b56b5a9edb77e228a4d908124e63fde
fharr <fhardanaldiansyah7@gmail.com>
2026-09-07 01:00:40 +0800

    Day6Coding

---
+++ DaySixCoding.java
@@ -0,0 +1,25 @@
+
+public class DaySixCoding {
+    public static void main(String[] args) {
+        //char adalah variabeluntuk karakter atau simbol 
+        //boolean itu variabel yang menentukan true atau false 
+        //hal yang perlu di ingat kalau untuk variabel char
+        //menggunakan tanda petik satu dan huruf Kapital
+        String NamaSaya = "Fhardan_Al";
+        int UmurSaatIni = 18;
+        float TinggiKu = 165.5f;
+        char GolonganDarah = 'B';
+        double danaSaatIni = 1.999999d;
+        boolean biodata = true;
+        boolean biodata1 = false;
+      
+        System.out.println("Perkenalkan\tNama\tSaya\tAdalah: " + NamaSaya);
+        System.out.println("Umurku\tSaat\tIni: " + UmurSaatIni);
+        System.out.println("Tinggi\tSaya\tMencapai : " + TinggiKu);
+        System.out.println("Golongan\tDarah: " + GolonganDarah);
+        System.out.println("biodata: " + true);
+        System.out.println("Dana: " + danaSaatIni);
+        
+    }
+    
+}
