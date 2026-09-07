1dd2062aafd27dc38e4d555883f74097965b9228
fharr <fhardanaldiansyah7@gmail.com>
2026-09-07 22:46:36 +0800

    Day7

---
+++ DaySeven.java
@@ -0,0 +1,61 @@
+
+public class DaySeven {
+    public static void main(String[] args) {
+        //char adalah tipe data untuk karakter atau simbol 
+        //boolean itu tipe data yang menentukan true atau false 
+        //hal yang perlu di ingat kalau untuk variabel char
+        //menggunakan tanda petik satu dan huruf Kapital
+        String NamaSaya = "Fhardan_Al";
+        int UmurSaatIni = 18;
+       final float TinggiKu = 165.5f;
+        char GolonganDarah = 'B';
+        double danaSaatIni = 1.999999d;
+        boolean biodata = true;
+        boolean biodata1 = false;
+        System.out.println("Perkenalkan\tNama\tSaya\tAdalah: " + NamaSaya);
+        System.out.println("Umurku\tSaat\tIni: " + UmurSaatIni);
+        System.out.println("Tinggi\tSaya\tMencapai : " + TinggiKu);
+        System.out.println("Golongan\tDarah: " + GolonganDarah);
+        System.out.println("biodata: " + true);
+        System.out.println("Dana: " + danaSaatIni);
+        System.out.println();
+        
+        System.out.println("\u001B[1m Pengenalan konsep dari tipe data String");
+       System.out.println("String harus menggunakan kutip dua"); 
+        //string merupakan tipe data yang bisa di input berupa teks, angka ataupun simbol
+        
+        String nama = "Faraa";
+        String pesan = "Haiiiii, selamat malam kakak ISC!";
+
+        // Penggabungan Stringnya
+        String salam = nama + " - " + pesan;
+
+        // Menampilkan hasilnya
+        System.out.println(salam);
+
+        // Menggunakan method bawaan length() untuk hitung panjang karakter
+        System.out.println("Panjang pesan: " + pesan.length());
+        
+        String contoh = "beberapa jenis atau fungsi dari string";
+        String contoh1 = "length(): Menghitung jumlah total karakter";
+        String contoh2 = "toUpperCase(): Mengubah semua huruf menjadi kapital.";
+        String contoh3 =  "toLowerCase(): Mengubah semua huruf menjadi huruf kecil.";
+        String contoh4 = "concat(): Menggabungkan dua teks.";
+        System.out.println();
+        
+        System.out.println("jenis-jenis nya: " + contoh);
+        System.out.println();
+        System.out.println("jenis 1= " + contoh1);
+        System.out.println();
+        System.out.println("jenis 2= " + contoh2);
+        System.out.println();
+System.out.println("jenis 3= " + contoh3);
+System.out.println();
+        System.out.println("jenis 4= " + contoh4);
+        
+        
+    }
+    }
+    
+
+                           
