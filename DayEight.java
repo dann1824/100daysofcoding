59f7aedb3f0e3dbf571bbeee3f43ed2afc0b493e
fharr <fhardanaldiansyah7@gmail.com>
2026-09-09 21:03:08 +0800

    day8

---
+++ DayEight.java
@@ -0,0 +1,38 @@
+
+public class DayEight {
+    
+    public static void main(String[] args) {
+       System.out.println("\u001B[1m Deklarasi, inisialisasi dan Update");
+        System.out.println();
+        
+        //deklrasai variabel tanpa mengisi nilai awal
+        String saya;
+        int angka;
+        double total_senilai;
+        
+        //memberi nilai pada variabel nya 
+         saya = "Mau Kaya";
+         angka = 100;
+        total_senilai = 989.59d;
+        
+        System.out.println("Saya awal: " + saya);
+        System.out.println("angka awal: " + angka);
+        System.out.println("total senilai awal: " + total_senilai);
+        System.out.println();
+        
+        //update nilai dari yang lama ke yang baru
+         saya = "Mauuu Sekaliiii KAYAAA";
+         angka = 999;
+         total_senilai = 1999.581d;
+        
+        System.out.println("Saya setelah di update: " + saya);
+        System.out.println("angka setelah di update: " + angka);
+        System.out.println("total senilai setelah di update: " + total_senilai);
+        
+        
+        
+        
+        
+    }
+    
+  }
