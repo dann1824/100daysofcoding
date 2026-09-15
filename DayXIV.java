cfce6344a93ee9b58de1be0dcaa3b9365ea36423
fharr <fhardanaldiansyah7@gmail.com>
2026-09-15 20:54:34 +0800

    day 14

---
+++ DayXIV.java
@@ -0,0 +1,22 @@
+
+public class DayXIV {
+    
+    public static void main(String[] args) {
+        
+        System.out.println("->>OPERASI ARITMATIKA<<-");
+        // untuk pertemuan sekarang penjumlahan dan pengurangan 
+        int a = 20;
+        int b = 30;
+        int Penjumlah;
+        int Pengurangan;
+        
+        Penjumlah = 20 + 30;
+        Pengurangan = 30 - 20;
+        
+        System.out.println("Hasil Jumlahnya : " + Penjumlah);
+        System.out.println("Hasil Pengurangan : " + Pengurangan);
+        
+        
+    }
+    
+}
