1ac0273150cbc8c2586275f98d26e278b4df9079
fharr <fhardanaldiansyah7@gmail.com>
2026-09-18 22:51:23 +0800

    day17

---
+++ DayXVII.java
@@ -0,0 +1,27 @@
+public class DayXVII {
+    public static void main(String[] args) {
+        // 1. Operator Dasar (=)
+        int angka = 20;
+        System.out.println("Nilai awal: " + angka); // Output: 20
+
+        // 2. Operator +=
+        angka += 10; // Sama dengan: angka = angka + 10
+        System.out.println("Setelah += 10: " + angka); // Output: 30
+
+        // 3. Operator -=
+        angka -= 3; // Sama dengan: angka = angka - 3
+        System.out.println("Setelah -= 3: " + angka); // Output: 27
+
+        // 4. Operator *=
+        angka *= 2; // Sama dengan: angka = angka * 2
+        System.out.println("Setelah *= 2: " + angka); // Output: 54
+
+        // 5. Operator /=
+        angka /= 4; // Sama dengan: angka = angka / 4
+        System.out.println("Setelah /= 4: " + angka); // Output: 13
+
+        // 6. Operator %=
+        angka %= 4; // Sama dengan: angka = angka % 4 (Sisa bagi  dibagi 4)
+        System.out.println("Setelah %= 4: " + angka); // Output: 1
+    }
+}
