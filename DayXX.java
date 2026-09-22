21f8d408bac6880c2d39cb185dc8f5b5442e18ec
fharr <fhardanaldiansyah7@gmail.com>
2026-09-22 21:46:35 +0800

    day20

---
+++ DayXX.java
@@ -0,0 +1,44 @@
+public class DayXX {
+    public static void main(String[] args) {
+        // 1. byte
+        byte b = 10;
+        String strByte = String.valueOf(b);
+        short s = 200;
+        String strShort = String.valueOf(s);
+        int i = 1000;
+        String strInt = String.valueOf(i);
+        long l = 50000L;
+        String strLong = String.valueOf(l);
+
+        
+        float f = 5.75f;
+        String strFloat = String.valueOf(f);
+        double d = 19.99d;
+        String strDouble = String.valueOf(d);
+
+        
+        boolean bool = true;
+        String strBoolean = String.valueOf(bool);
+        char c = 'A';
+        String strChar = String.valueOf(c);
+
+        // Contoh cetak hasil
+        System.out.println(strInt);
+        System.out.println(strByte);
+        System.out.println(strShort);
+        System.out.println(strLong);
+        System.out.println(strFloat);
+        System.out.println(strDouble);
+        System.out.println(strBoolean);
+        System.out.println(strChar);
+        
+        
+        
+        
+    
+        
+        
+        
+    }
+                           }
+          
