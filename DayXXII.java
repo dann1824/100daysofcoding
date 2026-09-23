2ae634d3c36a66e6b4bbd8519231029d215f64fe
fharr <fhardanaldiansyah7@gmail.com>
2026-09-23 23:36:35 +0800

    22day

---
+++ DayXXII.java
@@ -0,0 +1,41 @@
+
+public class DayXXII {
+    
+    public static void main(String[] args) {
+        
+        int x = 20;
+        int y = 30;
+        
+        System.out.println("->> CONTOH SEBELUM DI TUKAR <<-");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        
+        int temp = x;
+        x = y;
+        y = temp;
+        
+        System.out.println("+-+- SETELAH DI TUKAR +-+-");
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        
+        /**temp itu = temporary atau sementara
+         * temp hanya menyimpan data untuk sementara
+         * digunakan pada saat ingin menukar nilai
+         * temp ibarat 2 wadah 
+         * wadah 1 berisikan sabun 
+         * wadah 2 berisikan minyak 
+         * tentu kita tidak langsung menggabungkan
+         * maka kita sudah cukup dengan mengubah
+         * dari salah satu wadah tersebut 
+         * maka dari itu kita memerlukan
+         * wadah lainnya agar dapat di tukar
+         * tanpa bercampur
+         * itu adalah analogi sederhana 
+         * dari temp atau temporsry
+         */
+        
+        
+        
+    }
+    
+  }
